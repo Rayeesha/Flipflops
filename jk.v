b110d3daa1e802a3212ae110540ce236487018c4
RAYEESHA BHAT P <raycodes98@gmail.com>
2026-07-23 14:33:39 +0530

    Clean up header comments in jk.v
    Removed header comments from jk.v file.

--- jk.v
+++ jk.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 23.07.2026 10:34:43
-// Design Name: 
-// Module Name: jk
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module jk(input j,input k,input clk,input clr,output reg q);
 always@(posedge clr or negedge clk)
